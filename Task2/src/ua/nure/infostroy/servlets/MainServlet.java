fb914ad4bbe918f831237c0715f24c97110f5d9c
didevgen <didevgen@gmail.com>
2015-12-12 01:42:33 +0200

    Tried to make jstl tag - failed.

--- Task2/src/ua/nure/infostroy/servlets/MainServlet.java
+++ Task2/src/ua/nure/infostroy/servlets/MainServlet.java
@@ -7,8 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/*")
+@WebServlet("/app/*")
 public class MainServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req,resp);
+	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
---
+++ Task2/src/ua/nure/infostroy/taglibs/KaptchaTag.java
@@ -0,0 +1,13 @@
+package ua.nure.infostroy.taglibs;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class KaptchaTag extends SimpleTagSupport {
+	@Override
+	public void doTag() throws JspException, IOException {
+	}
+}
